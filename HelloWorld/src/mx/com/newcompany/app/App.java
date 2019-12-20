f76afa7fd7db545cfbd826bf4cf25e026404ae2d
Windows81 <Windows81@Server>
2019-12-19 22:18:01 -0600

    Change 3

--- HelloWorld/src/mx/com/newcompany/app/App.java
+++ HelloWorld/src/mx/com/newcompany/app/App.java
@@ -3,7 +3,7 @@ package mx.com.newcompany.app;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World 2");
+		System.out.println("Hello World 3");
 
 	}
 
